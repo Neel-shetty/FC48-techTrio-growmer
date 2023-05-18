7d1b5ee4bc7cd61edc0c9c7b166810a8c448d0e8
HarshithRajesh <harshithr2004@gmail.com>
2023-05-18 11:26:24 +0530

    host enabled

--- server/main.py
+++ server/main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 load_dotenv()
 
 app = Flask(__name__)
@@ -13,5 +15,6 @@ def home():
 
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True ,host="0.0.0.0")
