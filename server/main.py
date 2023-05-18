6dc41e7c0c5be448e3632029c41ace0d07c6556c
HarshithRajesh <harshithr2004@gmail.com>
2023-05-18 11:07:28 +0530

    Flask done

--- server/main.py
+++ server/main.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello word"
 
 
-if __main__ ==" __name__":
-    app.config(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
